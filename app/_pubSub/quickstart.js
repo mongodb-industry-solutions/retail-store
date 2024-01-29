1c0083533779c0007bdbbda82f116843bfb1152a
bereilhp <bereilhp@gmail.com>
2024-01-29 17:57:51 +0100

    push to MongoDB Atlas working

--- app/_pubSub/quickstart.js
+++ app/_pubSub/quickstart.js
@@ -1,27 +1,38 @@
 import { PubSub } from "@google-cloud/pubsub";
+import { connectToDatabase } from "../_db/connect";
+
+const db = await connectToDatabase();
 
 async function quickstart(
-  projectId = "ist-retail-demo", // Your Google Cloud Platform project ID
-  topicNameOrId = "my-topic", // Name for the new topic to create
-  subscriptionName = "my-sub" // Name for the new subscription to create
+  projectId = "ist-retail-demo", // Google Cloud Platform project ID from the GCP website
+  topicNameOrId = "my-topic", // Name for the new topic to be created
+  subscriptionName = "my-sub" // Name for the new subscription to be created
 ) {
-  // Instantiates a client
+  // Create a client
   const pubsub = new PubSub({ projectId });
 
-  // Creates a new topic
+  // Create a new topic
   const [topic] = await pubsub.createTopic(topicNameOrId);
   console.log(`Topic ${topic.name} created.`);
 
-  // Creates a subscription on that new topic
+  // Create a subscription on that new topic
   const [subscription] = await topic.createSubscription(subscriptionName);
 
-  // Receive callbacks for new messages on the subscription
-  subscription.on("message", (message) => {
+  // Listen for messages and save them on the DB
+  subscription.on("message", async (message) => {
     console.log("Received message:", message.data.toString());
-    process.exit(0);
+    try {
+      const jsonData = JSON.parse(message.data.toString()); // Assuming the data is in JSON 
+      const collection = db.collection("items");
+      const result = await collection.insertOne(jsonData);
+      console.log("Document inserted:", result.insertedId);
+    } catch (error) {
+      console.error("Error parsing or inserting JSON data:", error);
+    }
+    message.ack();
   });
 
-  // Receive callbacks for errors on the subscription
+  // Errors
   subscription.on("error", (error) => {
     console.error("Received error:", error);
     process.exit(1);
@@ -30,5 +41,7 @@ async function quickstart(
   // Send a message to the topic
   //topic.publishMessage({ data: Buffer.from("Test message!") });
 }
+
+quickstart();
 
 export { quickstart };
