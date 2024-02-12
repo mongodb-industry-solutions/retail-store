f2620c5b184e4ff54819632c5e42ec26fed7ed49
ainhoamugica <112406740+ainhoamugica@users.noreply.github.com>
2024-02-12 12:17:59 +0100

    Shop frontend

--- app/shop/page.js
+++ app/shop/page.js
@@ -1,5 +1,4 @@
 import Footer from "../_components/footer/Footer";
-import GetItem from "../_components/getItem/Getitem";
 import Navbar from "../_components/navbar/Navbar";
 import ProductCard from "../_components/productCard/ProductCard";
 import styles from "./shop.module.css";
@@ -8,17 +7,8 @@ export default function Page() {
   return (
     <>
       <Navbar></Navbar>
-      <GetItem />
 
       <div className={styles.products}>
-        <ProductCard
-          photo="/socks.png"
-          name="Leafy Socks"
-          brand="MongoDB"
-          elasticity="-1200"
-          price="35"
-        />
-
         <ProductCard
           photo="/socks.png"
           name="Leafy Socks"
