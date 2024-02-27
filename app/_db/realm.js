d0600ac760e81222bd755f38b43a21d8b741d0ac
bereilhp <bereilhp@gmail.com>
2024-02-27 16:13:30 +0100

    add api route to get image url from db

--- app/_db/realm.js
+++ app/_db/realm.js
@@ -1,6 +1,6 @@
 import * as Realm from "realm-web";
 
 export function useApp() {
-  const app = new Realm.App({ id: process.env.APP_ID });
+  const app = new Realm.App({ id: process.env.APP_ID2 });
   return app;
 }
