0e060654d192453c17ef5ea005326277bb0c143b
RamiPinto <rami.pinto@mongodb.com>
2024-03-14 17:46:24 +0100

    fix filters bug

--- app/api/getProducts/route.js
+++ app/api/getProducts/route.js
@@ -1,11 +1,21 @@
 import { NextResponse } from "next/server";
 import { connectToDatabase } from "../../_db/connect";
 
-export async function GET() {
+export async function POST(request) {
+    
+    const filters = await request.json();
+    
     const db = await connectToDatabase();
     const collection = db.collection("products");
+
+    let query = {};
+
+    if (filters.selectedBrands && filters.selectedBrands.length > 0) {
+        query = { brand: { $in: filters.selectedBrands } };
+    }
+
     const products = await collection
-        .find({ }, { projection: { name: 1, price: 1, brand: 1, image:1, id: 1, _id: 0 }})
+        .find(query, { projection: { name: 1, price: 1, brand: 1, image:1, id: 1, _id: 0 }})
         .toArray();
     return NextResponse.json({ products }, { status: 200 });
 }
