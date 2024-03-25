0555e6d2f674f849cbef649650198f2c5417bc33
bereilhp <bereilhp@gmail.com>
2024-03-25 19:00:19 +0100

    Working on the UI with the microservice start/stop

--- app/_components/productList/ProductList.jsx
+++ app/_components/productList/ProductList.jsx
@@ -1,29 +1,30 @@
-"use client"
+"use client";
 
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import ProductCard from "../productCard/ProductCard";
 import styles from "./productList.module.css";
+import PredPrice from "../predPrice/PredPrice";
 
-const ProductList = ({filters}) => {
+const ProductList = ({ filters }) => {
   const [products, setProducts] = useState([]);
-
 
   useEffect(() => {
     const fetchProducts = async () => {
       try {
-        const response = await axios.post('/api/getProducts', filters); 
-        const transformedProducts = response.data.products.map(product => ({
-          photo: product.image.url, 
+        const response = await axios.post("/api/getProducts", filters);
+        const transformedProducts = response.data.products.map((product) => ({
+          id: product.id,
+          photo: product.image.url,
           name: product.name,
           brand: product.brand,
           price: `${product.price.amount.toFixed(2)}`,
           pred_price: `${product.pred_price.toFixed(2)}`,
-          items: product.items
+          items: product.items,
         }));
         setProducts(transformedProducts);
       } catch (error) {
-        console.error('There was a problem with your fetch operation:', error);
+        console.error("There was a problem with your fetch operation:", error);
       }
     };
 
@@ -33,19 +34,18 @@ const ProductList = ({filters}) => {
   return (
     <div className={styles.productContainer}>
       {products.map((product, index) => (
-        <ProductCard
-          key={index} 
-          photo={product.photo}
-          name={product.name}
-          brand={product.brand}
-          price={product.price}
-          pred_price={product.pred_price}
-          items={product.items}
-          
-        />
+        <div key={index}>
+          <ProductCard
+            photo={product.photo}
+            name={product.name}
+            brand={product.brand}
+            price={product.price}
+            items={product.items}
+          />
+          <PredPrice productId={product.id} initialPredPrice={product.pred_price} />
+        </div>
       ))}
     </div>
   );
 };
-
 export default ProductList;
