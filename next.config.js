bf14d7c0396705ad4b233ae8cd3b1904c72bb4b5
bereilhp <bereilhp@gmail.com>
2024-03-28 14:53:04 +0100

    next.config.js

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    experimental: {
+        outputFileTracingIncludes: {
+          '/app/api/signURLs': ['./ist-retail-demo-c2e70dfceaa3.json'],
+        },
+      },
+}
 
 module.exports = nextConfig
