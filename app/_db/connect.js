00f827e4ffe56293d74ccbf3d4d0ef0e034fd540
bereilhp <bereilhp@gmail.com>
2024-04-01 21:15:07 +0200

    reuse db

--- app/_db/connect.js
+++ app/_db/connect.js
@@ -1,16 +1,24 @@
 import { MongoClient } from "mongodb";
 
-const uri = process.env.IST_SHARED_MONGODB; 
-
-const client = new MongoClient(uri);
+const uri = process.env.IST_SHARED_MONGODB;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 const dbName = "dotLocalStore";
+let db = null;
 
 export async function connectToDatabase() {
+  if (db) {
+    return db;
+  }
+
   try {
     await client.connect();
     //console.log("Connected successfully to MongoDB Atlas");
-    return client.db(dbName);
+    db = client.db(dbName);
+    return db;
   } catch (error) {
     console.error("Error connecting to MongoDB:", error);
     throw error;
