917890f88f4763f9faafe989808d917197619f3b
angie <angie.guemes@mongodb.com>
2024-09-24 15:42:54 -0600

    feature(ui): pagination to Shop page

--- app/_components/productList/ProductList.jsx
+++ app/_components/productList/ProductList.jsx
@@ -5,9 +5,16 @@ import axios from "axios";
 import ProductCard from "../productCard/ProductCard";
 import styles from "./productList.module.css";
 import PredPrice from "../predPrice/PredPrice";
+import Pagination from "@leafygreen-ui/pagination";
+
+const itemsPerPage = 10;
 
 const ProductList = ({ filters }) => {
-  const [products, setProducts] = useState([]);
+  const [filteredProducts, setFilteredProducts] = useState([]);
+  const [paginationLength, setPaginationLength] = useState(0)
+  const [currentPage, setCurrentPage] = useState(1);
+  const [firstIndex, setFirstIndex] = useState(0);
+  const [lastIndex, setLastIndex] = useState(itemsPerPage - 1);
 
   useEffect(() => {
     const fetchProducts = async () => {
@@ -22,7 +29,8 @@ const ProductList = ({ filters }) => {
           pred_price: `${product.pred_price.toFixed(2)}`,
           items: product.items,
         }));
-        setProducts(transformedProducts);
+        setFilteredProducts(transformedProducts);
+        setPaginationLength(transformedProducts.length)
       } catch (error) {
         console.error("There was a problem with your fetch operation:", error);
       }
@@ -31,20 +39,41 @@ const ProductList = ({ filters }) => {
     fetchProducts();
   }, [filters]);
 
+  useEffect(() => {
+    let firstIndex = (currentPage - 1) * itemsPerPage
+    let lastIndex = firstIndex + itemsPerPage
+    setFirstIndex(firstIndex)
+    setLastIndex(lastIndex)
+  }, [currentPage])
+
   return (
-    <div className={styles.productContainer}>
-      {products.map((product, index) => (
-        <div key={index}>
-          <ProductCard
-            id={product.id}
-            photo={product.photo}
-            name={product.name}
-            brand={product.brand}
-            price={product.price}
-            items={product.items}
-          />
-        </div>
-      ))}
+    <div>
+      <div className={styles.productContainer}>
+        {filteredProducts
+          .slice(firstIndex, lastIndex)
+          .map((product, index) => (
+            <div key={index}>
+              <ProductCard
+                id={product.id}
+                photo={product.photo}
+                name={product.name}
+                brand={product.brand}
+                price={product.price}
+                items={product.items}
+              />
+            </div>
+          ))}
+      </div>
+      <br></br>
+      <hr className={styles.hr}></hr>
+      <Pagination
+        currentPage={currentPage}
+        itemsPerPage={itemsPerPage}
+        itemsPerPageOptions={[8, 16, 32]}
+        numTotalItems={paginationLength}
+        onForwardArrowClick={ () => setCurrentPage(currentPage + 1) }
+        onBackArrowClick={ () => setCurrentPage(currentPage - 1) }
+      ></Pagination>
     </div>
   );
 };
