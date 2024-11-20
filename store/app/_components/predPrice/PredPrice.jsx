0ba3d7142773a4ff2337ed6e43d0d2f8e8c85e65
bereilhp <bereilhp@gmail.com>
2024-11-20 17:03:13 +0100

    Add change streams with sse

--- store/app/_components/predPrice/PredPrice.jsx
+++ store/app/_components/predPrice/PredPrice.jsx
@@ -1,38 +1,27 @@
 import React, { useState, useEffect } from "react";
-import axios from "axios";
 import style from "./predPrice.module.css";
-
 
 const PredPrice = ({ productId, initialPredPrice }) => {
   const [predPrice, setPredPrice] = useState(initialPredPrice);
-  const [prevPredPrice, setPrevPredPrice] = useState(initialPredPrice);
+  const [arrowDirection, setArrowDirection] = useState("");
 
   useEffect(() => {
-      const fetchUpdatedPredPrice = async () => {
-        try {
-          const response = await axios.post("/api/getPredPrice", { productId });
-          const newPredPrice = response.data.pred_price.toFixed(2);
-          setPrevPredPrice(predPrice);
-          setPredPrice(newPredPrice);
-         // console.log(newPredPrice);
-        } catch (error) {
-          console.error("Failed to fetch updated predicted price:", error);
-        }
-      };
-      
-      const intervalId = setInterval(fetchUpdatedPredPrice, 3000);
-
-      return () => clearInterval(intervalId);
-  }, [productId, predPrice]);
-
-  const arrowDirection = predPrice > prevPredPrice ? '/arrow_down.png' : predPrice < prevPredPrice ? '/arrow_up.png' : null;
+    setArrowDirection(
+      predPrice > initialPredPrice
+        ? "/arrow_down.png"
+        : predPrice < initialPredPrice
+        ? "/arrow_up.png"
+        : ""
+    );
+    setPredPrice(initialPredPrice);
+  }, [initialPredPrice]);
 
   return (
-
-   
-
     <div className={style.predPrice}>
-      Predicted Price: ${predPrice} {arrowDirection && <img className={style.arrowImg} src={arrowDirection} alt="Arrow" />}
+      Predicted Price: ${initialPredPrice}{" "}
+      {arrowDirection && (
+        <img className={style.arrowImg} src={arrowDirection} alt="Arrow" />
+      )}
     </div>
   );
 };
--- store/app/_components/productList/ProductList.jsx
+++ store/app/_components/productList/ProductList.jsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useCallback } from "react";
 import axios from "axios";
 import ProductCard from "../productCard/ProductCard";
 import styles from "./productList.module.css";
@@ -9,27 +9,70 @@ import Pagination from "@leafygreen-ui/pagination";
 const itemsPerPage = 20;
 
 const ProductList = ({ filters }) => {
-  const [filteredProducts, setFilteredProducts] = useState([]);
-  const [paginationLength, setPaginationLength] = useState(0)
+  const [sseConnection, setSSEConnection] = useState(null);
+  const [filteredProducts, setFilteredProducts] = useState({});
+  const [paginationLength, setPaginationLength] = useState(0);
   const [currentPage, setCurrentPage] = useState(1);
   const [firstIndex, setFirstIndex] = useState(0);
   const [lastIndex, setLastIndex] = useState(itemsPerPage - 1);
+
+  const listenToSSEUpdates = useCallback(() => {
+    console.log("listenToSSEUpdates func");
+    const eventSource = new EventSource("/api/sse");
+
+    eventSource.onopen = () => {
+      console.log("SSE connection opened.");
+      // Save the SSE connection reference in the state
+    };
+
+    eventSource.onmessage = (event) => {
+      const data = JSON.parse(event.data);
+      console.log("Received SSE Update:", data);
+      // validate if the change was on the pred_price field
+      if (data.updateDescription.updatedFields["pred_price"]) {
+        const objectId = data.documentKey._id;
+        const newPredPrice = data.updateDescription.updatedFields.pred_price;
+        console.log(
+          `the pred price changed to ${newPredPrice} for product with id ${objectId}`
+        );
+        setFilteredProducts((prevData) => {
+          prevData[objectId] = {
+            ...prevData[objectId],
+            pred_price: newPredPrice,
+          };
+          return { ...prevData };
+        });
+      }
+    };
+
+    eventSource.onerror = (event) => {
+      console.error("SSE Error:", event);
+    };
+    setSSEConnection(eventSource);
+    return eventSource;
+  }, []);
 
   useEffect(() => {
     const fetchProducts = async () => {
       try {
         const response = await axios.post("/api/getProducts", filters);
-        const transformedProducts = response.data.products.map((product) => ({
-          id: product.id,
-          photo: product.image.url,
-          name: product.name,
-          brand: product.brand,
-          price: `${product.price.amount.toFixed(2)}`,
-          pred_price: `${product.pred_price.toFixed(2)}`,
-          items: product.items,
-        }));
+        let transformedProducts = {};
+        console.log("/api/getProducts", response.data.products);
+        response.data.products.map(
+          (product) =>
+            (transformedProducts[product._id] = {
+              _id: product._id,
+              id: product.id,
+              photo: product.image.url,
+              name: product.name,
+              brand: product.brand,
+              price: `${product.price.amount.toFixed(2)}`,
+              pred_price: `${product.pred_price.toFixed(2)}`,
+              items: product.items,
+            })
+        );
         setFilteredProducts(transformedProducts);
-        setPaginationLength(transformedProducts.length)
+        setPaginationLength(Object.keys(transformedProducts).length);
       } catch (error) {
         console.error("There was a problem with your fetch operation:", error);
       }
@@ -39,16 +82,27 @@ const ProductList = ({ filters }) => {
   }, [filters]);
 
   useEffect(() => {
-    let firstIndex = (currentPage - 1) * itemsPerPage
-    let lastIndex = firstIndex + itemsPerPage
-    setFirstIndex(firstIndex)
-    setLastIndex(lastIndex)
-  }, [currentPage])
+    let firstIndex = (currentPage - 1) * itemsPerPage;
+    let lastIndex = firstIndex + itemsPerPage;
+    setFirstIndex(firstIndex);
+    setLastIndex(lastIndex);
+  }, [currentPage]);
+
+  useEffect(() => {
+    const eventSource = listenToSSEUpdates();
+
+    return () => {
+      if (eventSource) {
+        eventSource.close();
+        console.log("SSE connection closed.");
+      }
+    };
+  }, [listenToSSEUpdates]);
 
   return (
     <div>
       <div className={styles.productContainer}>
-        {filteredProducts
+        {Object.values(filteredProducts)
           .slice(firstIndex, lastIndex)
           .map((product, index) => (
             <div key={index}>
@@ -58,6 +112,7 @@ const ProductList = ({ filters }) => {
                 name={product.name}
                 brand={product.brand}
                 price={product.price}
+                pred_price={product.pred_price}
                 items={product.items}
               />
             </div>
@@ -68,10 +123,10 @@ const ProductList = ({ filters }) => {
       <Pagination
         currentPage={currentPage}
         itemsPerPage={itemsPerPage}
-        //itemsPerPageOptions={[8, 16, 32]}
+        itemsPerPageOptions={[8, 16, itemsPerPage]}
         numTotalItems={paginationLength}
-        onForwardArrowClick={ () => setCurrentPage(currentPage + 1) }
-        onBackArrowClick={ () => setCurrentPage(currentPage - 1) }
+        onForwardArrowClick={() => setCurrentPage(currentPage + 1)}
+        onBackArrowClick={() => setCurrentPage(currentPage - 1)}
       ></Pagination>
     </div>
   );
