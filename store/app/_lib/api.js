9ba12d537e12274ae674dbe72b04362bab722841
angie <angie.guemes@mongodb.com>
2024-11-22 16:43:44 -0600

    search(api): add atlas search api and score in ui

--- store/app/_lib/api.js
+++ store/app/_lib/api.js
@@ -1,5 +1,24 @@
-export async function getProductsWithSearch(query, filters = {}) {
+export async function getProductsWithSearch(query = '', filters = {}) {
+    console.log('getProductsWithSearch')
     const response = await fetch(`/api/search`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({query, filters}),
+    });
+    if (!response.ok) {
+      console.log(response)
+      throw new Error(`Error fetching products: ${response.status}`);
+    }
+    const data = await response.json();
+    console.log('data: ', data)
+    return data.products;
+  }
+
+  export async function getProductsWithVectorSearch(query, filters = {}) {
+    console.log('getProductsWithVectorSearch')
+    const response = await fetch(`/api/vectorSearch`, {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
