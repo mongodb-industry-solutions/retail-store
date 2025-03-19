a83b25d810940d7ae0422565dcad5b0fc2dd0ab6
floarin <florencia.arin@mongodb.com>
2025-03-19 16:13:42 +0100

    adding tal track to vector search

--- store/app/_lib/talkTrack.js
+++ store/app/_lib/talkTrack.js
@@ -33,105 +33,79 @@ const vectorSearchSection = {
     heading: "Vector Search",
     content: [
         {
-            heading: 'Product Description',
-            body: 'While product onboarding as a whole encompasses many steps in this demo we will focus solely on the description-writing process. When onboarding a new product, retailers must craft a description that accurately aligns with the product image before deploying it to their e-commerce portal. '
-        },
-        {
-            heading: "The power of a good description",
-            body: `<p>Having a great description can enhance user engagement and satisfaction as  
-            <a href="https://www.businessdasher.com/importance-of-product-description/?utm_source=chatgpt.com" target="_blank"> 87% of online shoppers consider product descriptions to be crucial when making a 
-            buying decision</a>. On the other hand, having inaccurate descriptions can result in 
-            revenue loss and trust decline with your customer base as <a href="https://www.ax-semantics.com/en/blog/product-descriptions?utm_source=chatgpt.com" target="_blank">40% of consumers have 
-            returned online purchases due to poor product content</a>.</p>`,
+            heading: 'Vector search for semantic searches',
+            body: '<p>Traditional search engines match exact words, but in an e-commerce catalog, shoppers often search in ways that don’t perfectly align with product names.</p><p>Instead of matching exact words, Vector Search leverages precomputed embeddings of the product catalog. When a user performs a semantic search, their query is processed by an AI model to generate a corresponding vector embedding.</p><p>Vector Search then compares this query embedding against the existing product embeddings, retrieving the most relevant matches based on meaning rather than exact text. The closer two vectors are (the search query and the product embedding), the more similar their meaning.</p>',
             isHTML: true
         },
         {
-            heading: "Challenges & considerations",
-            body: [
-                'Structuring product descriptions that addresses their target’s needs and desires while maintaining a consistent tone across the platform.',
-                'Creating SEO-optimized product descriptions to drive organic traffic and improve search engine rankings.',
-                'For multilingual portals and with multiple operating geographies, this challenge of accuracy increases.',
-                'Even after a description is written, too often there is still a content approval process that needs to happen, delaying the process.'
-            ],
+            heading: "Why it matters for e-commerce?",
+            body: `<p>Vector Search goes beyond simple keyword matching—it leads to more relevant search results, smarter recommendations, and a better shopping experience.</p><p>By storing product embeddings in MongoDB, we can fine-tune searches, ensuring users see both relevant results and highlighted products. This enhances personalization, product discovery, and conversions.</p>`,
+            isHTML: true
         },
+        
     ],
 }
-const behindTheScenesSection = {
+const behindTheScenesSectionVectorSearch = {
     heading: "Behind the Scenes",
     content: [
         {
-            heading: "Architecture overview",
-            body: "",
-        },
-        {
-            image: {
-                src: "/talkTrack/Architecture.png",
-                alt: "Architecture",
-            },
-        },
-        {
-            heading: "",
-            body: `<p>Lets take a look at our architecture. At the core we have our database, 
-            which contains the product catalog of our e-commerce store. We also have an object 
-            storage where the unstructured data lives. In this demo, that is our image files 
-            for our catalog. This could be for example an S3, a Google Storage bucket or an 
-            Azure Blob storage.</p> 
-            <p>The flow starts at the left hand side, when a new product is received. This 
-            can be a manual input or an automated batched event. Once we get the new product 
-            on the system retailers can generate the description by sending a query to TogetherAI's 
-            endpoint. We are using their Vision Models that can process and understand images 
-            alongside text. These models combine <strong>computer vision and natural language processing</strong>.</p> 
-            <p>Inside the request we specify which LLM type we want to use, the length, the languages 
-            in which we want this description and of course the product's image URL so the model can process it.</p>
-            <p>Then, TogetherAI takes this data and uses one of its Llama3 <a href="https://www.together.ai/models" target="_blank">vision models</a> 
-            to scan the image and generate a description that matches the specified requirements. And it returns the 
-            descriptions to the product description application.</p>
-            <p>The product along with its description will be then updated in real time inside the catalog. 
-            And at any point in time the UX writing team can enhance these descriptions from inside the application 
-            and this will be updated on the catalog.</p>
+            heading: "Understanding Embeddings",
+            body: `
+                <p>Embeddings are numerical representations of words, images, or concepts that allow computers to understand their meaning in a vector space. Instead of relying on exact words, embeddings capture relationships and similarities between things.</p>
             `,
             isHTML: true
         },
         {
-            heading: "",
-            body: "The main tech stack components can be found below.",
+            image: {
+                src: "/images/VectorSearchEmbeddings.png",
+                alt: "Semantic Search with Vector Search"
+            },
         },
         {
-            heading: "",
-            body: [
-                "MongoDB Atlas for the database.",
-                "Togeteher.AI for generating the products descriptions using their available chat LLMs",
-                "S3 buckets. This can be any file storage system. Such as: GCP buckets, Azure containers or AWS S3 buckets",
-                "GC Virtual Machine. The deployed app of this demo is deployed on a GC virtual machine.",
-                "Next.js App Router for the framework"
-            ],
+            heading: "How Does Vector Search Work?",
+            body: `
+                
+                <p>In the illustration, each animal is stored with a vector that represents its characteristics—like <strong>type of animal (x-axis) and color (y-axis)</strong>.</p>
+                <p>When searching for <strong>"A very white cat"</strong>, the system <strong>converts the query into a vector using an AI model</strong> and then uses <strong>Vector Search</strong> to find the closest stored vectors in <strong>MongoDB</strong>.</p>
+                <p>The results follow a simple rule: the closer an animal's vector is to the query vector, the more similar it is in meaning. That’s why an <em>orange cat</em> appears closer to the white cat than a black dog.</p>
+                <p>In <strong>Vector Search</strong>, this same logic applies—each product in the catalog is stored with its embedding, and searches return the most relevant results based on vector similarity, not just text matches.</p>
+                 <p><strong>Clarification: This is a Simplified Representation</strong></p>
+                 <p><strong>High dimensionality:</strong> Embeddings exist in spaces with hundreds or thousands of dimensions. Dimensionality reduction techniques help optimize them without losing meaning, but they are never just two-dimensional.</p>
+                 <p><strong>Vector ≠ Embedding:</strong> A vector is a mathematical structure, while an embedding is an optimized representation that captures complex relationships in data.</p>
+                 <p><strong>Individual numbers in an embedding don’t have a literal meaning:</strong> They emerge from statistical patterns learned by the model. Interpreting each dimension in isolation doesn’t make sense—what matters is how embeddings relate to each other.</p>
+            `,isHTML: true
         },
+       
     ],
 }
 const whyMDBSection = {
     heading: "Why MongoDB?",
     content: [
         {
-            heading: "MongoDB + TogetherAI",
+            heading: "Versatile: All Data in One Place",
             body: `
-                Product onboarding to a retail e-commerce portal is a time-consuming effort for many retailers. They need to ensure they’ve created a product description that matches the image, then deploy it to their e-commerce portal. For multilingual portals and multiple operating geographies, this challenge of accuracy increases. With Together AI’s support for multimodal models (e.g. Llama 3.2) and MongoDB Atlas’s vector embeddings, we can create accurate product descriptions in multiple languages.
+                <p>MongoDB allows you to store <strong>all types of data together</strong>, including product details, geospatial data, and AI-generated embeddings.</p>
+                <p>Instead of managing separate databases for different data types, MongoDB keeps everything in one place, making your system <strong>more efficient and easier to manage</strong>.</p>
             `,
+            isHTML: true
         },
         {
-            heading: "Embeddings and inference with Together AI",
-            body: "Together AI generated product descriptions based on images retrieved from the product catalog using Llama 3.2 vision models. This way, each product’s unique characteristics were considered, then generated in multiple languages. These descriptions could then be embedded into the MongoDB Atlas Vector Search database via a simple API."
+            heading: "Flexible: Schema Flexibility Without Downtime",
+            body: `
+                <p>Product catalogs change constantly, requiring updates to descriptions, attributes, and categories.</p>
+                <p>MongoDB’s <strong>dynamic schema</strong> allows these modifications <strong>without downtime</strong>, ensuring uninterrupted service.</p>
+                <p>This flexibility is critical for e-commerce platforms that need to scale and adapt quickly to market demands.</p>
+            `,
+            isHTML: true
         },
         {
-            heading: "Indexed embeddings with MongoDB Atlas Vector Search",
-            body: "Using MongoDB Atlas Vector Search capabilities, we could create embeddings (as an extended phase of this architecture), and then indexed them to be used to retrieve relevant data based on other matched product queries."
-        },
-        {
-            heading: "Real-time data processing",
-            body: "By connecting this setup to a real-time product dataset, we ensured that product descriptions in multiple languages were always updated automatically. So when a marketplace vendor or retailer uploads new images with distinct characteristics, they get up-to-date product descriptions in the catalog."
-        },
-        {
-            heading: "Mitigation of vendor lock-in risk",
-            body: "The fully managed multi-cloud database service, supports deployments across major cloud providers such as AWS, Azure, and Google Cloud. This autonomy allows businesses to adapt more readily to evolving technological landscapes "
+            heading: "Easy: Developer-Friendly & Intuitive",
+            body: `
+                <p>MongoDB’s intuitive <strong>document model</strong> makes working with complex data structures simple.</p>
+                <p>Developers can focus on <strong>innovation</strong> rather than dealing with rigid schemas or complex queries.</p>
+                <p>By integrating embeddings and operational data in a single platform, MongoDB <strong>streamlines AI-powered search and personalization</strong> without additional overhead.</p>
+            `,
+            isHTML: true
         },
     ],
 }
@@ -294,67 +268,53 @@ export const shopPageDynamicPricing = [
 ]
 export const shopPageVectorSearch = [
     vectorSearchSection,
-    behindTheScenesSection,
+    behindTheScenesSectionVectorSearch,
     {
-        heading: "How to demo",
+        heading: "How to Demo",
         content: [
             {
-                heading: "Understanding this page",
-                body: `<p>This page contains the product catalog of an e-comerce store.
-                    There are two <img src="/icons/talkTrackIcon.png" alt="TalkTrackWand"/> 
-                    talk track buttons in this page. This one contains the information about 
-                    the Vector Search feature and the one next to "Open Store" contains 
-                    the information on the Dynamic Price feature.</p>    
-                `,
-                isHTML: true
-            },
-            {
-                heading: "How to demo this page",
-                body: ''
-
-            },
-            {
-                heading: "",
-                body: 'Understanding the "Description Generator” page'
-
-            },
-            {
-                heading: "",
-                body: [
-
-                    {
-                        heading: "You have 4 options in which you can select a product to generate the description",
-                        body: [
-                            'First, by pasting the ObjectId of a product inside the input field and clicking on “Upload”.',
-                            'Second one, by clicking on the Sprinkle button inside the catalog.',
-                            'Third one, by clicking on “Use sample image from catalog” this will always load the same product sample image of a shoe. ',
-                            'And lastly, by clicking on “Upload product image” this will allow you to do the demo with an image that you Upload. Take advantage of this by uploading a product similar to what your prospect would like to see.'
-                        ]
-                    },
-                    {
-                        heading: "You will see the image of the product displayed inside the dotted rectangle.",
-                        body: []
-                    },
-                    {
-                        heading: "Select the Model, Languages (max 3) and Length you want for your descriptions. Note that En, Sp, Fr will be faster to generate than other languages.",
-                        body: []
-                    },
-                    {
-                        heading: "Click on “Generate descriptions",
-                        body: []
-                    },
-                    {
-                        heading: "You will see on the right side the descriptions generated for the selected languages, length and model",
-                        body: []
-                    },
-                    {
-                        heading: "Go back to the “Product Catalog” and you will see the descriptions that were uploaded to the catalog. Just make sure filters align to what you selected when generating the descriptions. To go directly to that product inside the catalog use the button as a shortcut.",
-                        body: []
-                    }
-                ]
-
+                heading: "Demo Steps",
+                body: `
+                <p><strong>Step 1:</strong> Navigate to the <strong>Shop</strong> screen.</p>
+                <p><strong>Step 2:</strong> In the search bar, type <strong>"Kitchen appliances"</strong>. 
+                The products that load are retrieved using <strong>Atlas Search</strong> based on 
+                product names. Notice that the top results are food items rather than appliances 
+                because they contain the word "kitchen" in their name, but they don’t truly match 
+                what the user is looking for.</p>
+            `,
+            isHTML: true
+        },
+        {
+            image: {
+                src: "/images/KitchenSearchExample_1.png",
+                alt: "Atlas Search Results Example"
             }
-        ],
+        },
+        {
+            body: `
+                <p><strong>Step 3:</strong> Enable <strong>Vector Search</strong> by turning on the toggle next to the 
+                input field and pressing <strong>Enter</strong> to search again. Now, the top results 
+                are much closer to actual kitchen appliances.</p>
+            `,
+            isHTML: true
+        },
+        {
+            image: {
+                src: "/images/KitchenSearchExample_2.png",
+                alt: "Vector Search Results Example"
+            }
+        },
+        {
+            body: `
+                <p><strong>Step 4:</strong> Try the following example queries using Vector Search:</p>
+                <p>House decoration<br>Kitchen appliances<br>Gift for a kid<br>Clothes for winter</p>
+                <p>These examples demonstrate the improved relevance of Vector Search compared to 
+                standard Atlas Search, providing a clearer understanding of how AI-driven search enhances 
+                the user experience.</p>
+            `,
+            isHTML: true
+            }
+        ]
     },
     whyMDBSection
-]
+];
